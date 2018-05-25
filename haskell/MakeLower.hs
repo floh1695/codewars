2f3e213aa860df1a0eab45ce40a02177c299d107
Charles Bayley <cbayley95@gmail.com>
2018-05-25 02:58:59 -0400

    Added all files from `cabal init`

--- haskell/MakeLower.hs
+++
@@ -1,9 +0,0 @@
-module MakeLower where
--- MakeLowerCase
--- https://www.codewars.com/kata/makelowercase/train/haskell
-
-import Data.Char
-
-makeLowerCase :: [Char] -> [Char]
-makeLowerCase = map toLower
-
