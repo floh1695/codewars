f4a361cf0437bbc980b5c229b8d8cd639a8d9c14
Charles Bayley <cbayley95@gmail.com>
2018-05-25 02:15:06 -0400

    Added MakeLower and its test

---
+++ haskell/Test.MakeLower.hs
@@ -0,0 +1,10 @@
+import MakeLower
+import Test.Hspec
+import Test.QuickCheck
+
+main :: IO ()    -- This says that main is an IO action.
+main = hspec $ do
+  describe "The makeLowerCase function" $ do
+    it "should work for some examples" $ do
+      makeLowerCase "HELLO" `shouldBe` "hello"
+
