e1beebad51194368bed7c6dddfa88141c3a3ea8c
Charles Bayley <cbayley95@gmail.com>
2018-05-25 02:16:06 -0400

    Moved source files into src

---
+++ haskell/src/MakeLower.hs
@@ -0,0 +1,9 @@
+module MakeLower where
+-- MakeLowerCase
+-- https://www.codewars.com/kata/makelowercase/train/haskell
+
+import Data.Char
+
+makeLowerCase :: [Char] -> [Char]
+makeLowerCase = map toLower
+
