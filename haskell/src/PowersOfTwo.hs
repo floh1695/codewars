7e7ff42975690608ce6d620baeb8286e48fd5e25
Charles Bayley <cbayley95@gmail.com>
2018-05-27 21:46:37 -0400

    Added another solution for the last problem

--- haskell/src/PowersOfTwo.hs
+++ haskell/src/PowersOfTwo.hs
@@ -2,11 +2,20 @@ module PowersOfTwo where
 -- Powers of 2
 -- https://www.codewars.com/kata/powers-of-2
 
+-- First solution
+-- powersOfTwo :: Int -> [Int]
+-- powersOfTwo n
+--   | n == 0         = singleton
+--   | otherwise      = powersOfTwo' ++ singleton
+--   where
+--     powersOfTwo'   = powersOfTwo (n - 1)
+--     n'             = 2.0 ** (fromIntegral n)
+--     singleton      = [floor n']
+
+-- Using list comprehension
+-- powersOfTwo :: Int -> [Int]
+-- powersOfTwo n = [ 2^x | x <- [0 .. n] ]
+
+-- Second solution
 powersOfTwo :: Int -> [Int]
-powersOfTwo n
-  | n == 0         = singleton
-  | otherwise      = powersOfTwo' ++ singleton
-  where
-    powersOfTwo'   = powersOfTwo (n - 1)
-    n'             = 2.0 ** (fromIntegral n)
-    singleton      = [floor n']
+powersOfTwo n = map (2 ^) [0..n]
